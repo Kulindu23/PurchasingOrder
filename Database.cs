ecc75267b2459575b71d6b3f5f5bb7c0c4545254
Kulindu Senura <kulindusenu2002@gmail.com>
2026-04-23 11:23:04 +0530

    Complete_Project_22_04_26

---
+++ Database.cs
@@ -0,0 +1,37 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+
+namespace Purchasing_Order
+{
+    class Database
+    {
+        public SqlConnection con;
+        public SqlCommand cmd;
+        public SqlDataAdapter da;
+        public DataTable dt;
+        public SqlDataReader dr;
+
+        public Database()
+        {
+            con = new SqlConnection("Data Source=DESKTOP-969UD0H;Initial Catalog=KS23;Integrated Security=True;");
+        }
+        public void OpenConnection()
+        {
+            con.Open();  
+        }
+        public void CloseConnection()
+        {
+            con.Close();
+        }
+        public DataTable GetData(string a)
+        {
+            OpenConnection();
+            da = new SqlDataAdapter(a, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            CloseConnection();
+            return dt;
+        }
+    }
+}
